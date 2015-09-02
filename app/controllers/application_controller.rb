56ec87d436a5dd988c3e231e9cc07e84506636af
jordananders <jordan.anders@gmail.com>
2015-09-02 22:59:48 +0000

    Basic Oauth2 server.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :doorkeeper_authorize!, only: :me
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def me
+    render json: User.find(doorkeeper_token.resource_owner_id).as_json
+  end
 end
