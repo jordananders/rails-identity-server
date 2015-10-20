68d2a3dd667e15106123efe6884f8d97a2f45d06
jordananders <jordan.anders@gmail.com>
2015-10-20 21:51:46 +0000

    Another user model fix.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   
-  class_attribute :uuid
-  self.uuid ||= SecureRandom.uuid
-  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :lockable, :confirmable
+         
+    before_save :set_uuid
+    def set_uuid
+        self.uuid ||= SecureRandom.uuid
+    end
 end
