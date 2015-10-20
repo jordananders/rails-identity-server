bb908c47113c57520408dd6d9f9771d0685d7e08
jordananders <jordan.anders@gmail.com>
2015-10-20 21:21:49 +0000

    User model fix.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   
+  class_attribute :uuid
   self.uuid ||= SecureRandom.uuid
   
   devise :database_authenticatable, :registerable,
