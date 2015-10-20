f104a9d78b73f678edf3e41e51452f609e0efaad
jordananders <jordan.anders@gmail.com>
2015-10-20 21:07:26 +0000

    Fixing uuid generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,10 @@ class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+  self.uuid ||= SecureRandom.uuid
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :lockable, :confirmable
-         
-  validates :uuid, presence: true
-  before_validation :ensure_uuid
-  def ensure_uuid
-      self.uuid ||= SecureRandom.uuid
-  end
 end
