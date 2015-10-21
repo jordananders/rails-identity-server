fdb3ee74dbc452813e77d241184c25aef6dd2986
jordananders <jordan.anders@gmail.com>
2015-10-21 01:38:48 +0000

    Configure action mailer default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,11 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Don't care if the mailer can't send.
+  #config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { :host => ENV['ACTION_MAILER_URL'] }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
